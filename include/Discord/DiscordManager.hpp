fe92a08d7bf625f214a1877ac7c935c4d099e99c
landingpad75 <skydevsmc@outlook.com>
2025-11-25 00:01:48 +0100

    quick include fix

--- include/Discord/DiscordManager.hpp
+++ include/Discord/DiscordManager.hpp
@@ -1,4 +1,14 @@
 #pragma once
+
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#endif
+
 #include <discord.h>
 #include <string>
 #include <functional>
